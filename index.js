3e59b8f084fc596510f59957010e7a9f0c303588
Daniel Zhang <dzhang55@gmail.com>
2016-10-09 23:56:03 -0400

    log request

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var index = 0;
 
 var token = "LL6Jo1Y1Z6KYcGQNMpQpv6n8";
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,7 +22,9 @@ app.get('/', function(request, response) {
 });
 
 app.post('/tictactoe', function(request, response) {
-  console.log(request.text);
+  index++;
+  console.log(index);
+  response.send("hello " + index);
 });
 
 app.listen(app.get('port'), function() {
