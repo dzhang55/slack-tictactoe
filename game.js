41c5bb2028741719aad81559ca566bc017296947
Daniel Zhang <dzhang55@gmail.com>
2016-10-10 12:17:21 -0400

    basic functionality, game logic untested

--- game.js
+++ game.js
@@ -1,50 +1,118 @@
-const DIMENSION = 3;
+var DIMENSION = 3;
 
 var Game = function(playerX, playerO) {
 	this.playerX = playerX;
 	this.playerO = playerO;
-	this.board = new Array[DIMENSION][DIMENSION];
+	this.board = new Array(DIMENSION);
+  for (var i = 0; i < DIMENSION; i++) {
+    this.board[i] = new Array(DIMENSION);
+    for (var j = 0; j < DIMENSION; j++) {
+      this.board[i][j] = 0;
+    }
+  }
   this.sentStartMessage = false;
   this.xTurn = false;
+  this.numMoves = 0;
+  this.over = false;
+  this.winner;
 }
 
-Game.prototype.didPlayerWin = function(player, board) {
+// Check if a player won the game.
+Game.prototype.didPlayerWin = function(player) {
   var win = false;
   for (var i = 0; i < DIMENSION; i++) {
     // Check horizontal row i.
-    win = win || isInARow([i, 0], [1, 0], board, player);
+    win = win || isInARow([i, 0], [1, 0], player);
 
     // Check vertical column i.
-    win = win || isInARow([0, i], [0, 1], board, player);
+    win = win || isInARow([0, i], [0, 1], player);
   }
   // Check diagonals.
-  win = win || isInARow([0, 0], [1, 1], board, player);
-  win = win || isInARow([2, 0], [-1, -1], board, player);
+  win = win || isInARow([0, 0], [1, 1], player);
+  win = win || isInARow([2, 0], [-1, -1], player);
   return win;
 }
 
-Game.prototype.isInARow = function(start, vector, board, player) {
+// Check a given direction for a win.
+Game.prototype.isInARow = function(start, vector, player) {
   for (var i = 0; i < DIMENSION; i++) {
     var row = start[0] + i * vector[0];
     var col = start[1] + i * vector[1];
-    if (board[row][col] != player) {
+    if (this.board[row][col] !== player) {
       return false;
     }
   }
   return true;
 }
 
+// Check if the move can be made.
+Game.prototype.validMove = function(move) {
+  var row = parseInt(move[0]);
+  var col = parseInt(move[1]);
+  if (row < 0 || row > DIMENSION - 1 || col < 0 || col > DIMENSION - 1) {
+    return false;
+  }
+  return this.board[row][col] === 0;
+}
+
+// Check if the user is in the game.
+Game.prototype.isValidUser = function(user) {
+  return user === this.playerX || user === this.playerO;
+}
+
+// Check if it is this player's turn.
+Game.prototype.isPlayersTurn = function(player) {
+  return this.xTurn ? player === this.playerX : player === this.playerO;
+}
+
+// Make a move and check win conditions.
+Game.prototype.move = function(move, player) {
+  this.numMoves++;
+  if (player === this.playerX) {
+    this.board[row][col] = -1;
+  }
+  else if (player === this.playerO) {
+    this.board[row][col] = 1;
+  }
+  if (this.didPlayerWin(player)) {
+    this.over = true;
+    this.winner = player;
+  }
+  else if (this.numMoves === DIMENSION * DIMENSION) {
+    this.over = true;
+    this.winner = 'TIE';
+  } else {
+    this.xTurn = !this.xTurn;
+  }
+}
+
+Game.prototype.over = function() {
+  return this.over;
+}
+
+// Output a string representation of the game state.
 Game.prototype.toString = function() {
   var output = '```';
-  if (!sentStartMessage) {
+  if (!this.sentStartMessage) {
     output += 'Welcome to Tic Tac Toe!\n';
-    sentStartMessage = true;
+    this.sentStartMessage = true;
   }
   output += '| X | O | O |\n|---+---+---|\n| O | X | X |\n|---+---+---|\n' + 
     '| X | O | X |\n';
-  output += this.xTurn ? this.playerX : this.playerO + '\'s turn';
+  if (this.over) {
+    output += 'Game over! Winner: ' + this.winner;
+  } else {
+    output += this.xTurn ? this.playerX : this.playerO + '\'s turn';
+  }
   output += '```';
   return output;
+}
+
+// Update game state to reflect player's forfeit.
+Game.prototype.forfeit = function(player) {
+  this.over = true;
+  this.winner = player === this.playerX ? this.playerO : this.playerX;
+  return this.toString();
 }
 
 module.exports = Game;
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 var games = {};
 
 var token = "LL6Jo1Y1Z6KYcGQNMpQpv6n8";
+var teamID = "T2GQDU57C";
+
+var userToken = "xoxp-84829957250-84812860336-89507551990-8f567ead26de8d5e4ef8571a50228cef";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,62 +18,73 @@ app.use(bodyParser.urlencoded({
 
 app.post('/tictactoe', function(request, response) {
   var body = request.body;
+  var channel = body.channel;
   var message = body.text;
-  console.log(body);
-  //console.log(message);
-  if (body.token != token) {
+
+  if (!isValid(body)) {
     throw err;
   }
   if (isUser(message)) {
-    if (body.channel in games) {
+    if (channel in games) {
       response.send("This channel already has a game!");
     } else {
       // Remove '@' symbol.
       var challengedUser = message.slice(1);
       games[channel] = new Game(challengedUser, body.user_name);
-      response.send(games[channel].toString());
-      // challenge user to game
+      sendChannelResponse(response, games[channel].toString());
     }
   }
   else if (isHelp(message)) {
-    // send instructions
+    response.send('```Welcome to Tic Tac Toe! Here are the available ' + 
+      'commands:\n/ttt [@user] - to start a game\n/ttt [0-2] [0-2] - to' +
+      ' make a move\n/ttt forfeit - to forfeit the game```');
+  }
+  else if (!(channel in games)) {
+    response.send('This channel does not have a game! Use /ttt [@user] to ' + 
+      'start a game.');
+  }
+  else if (!validUser(games[channel], body.user_name)) {
+    response.send('You aren\'t in this game.');
   }
   else if (isMove(message)) {
-    if (body.channel in games) {
-      var move = parseMove(message);
-      updateBoard(move, body.user_id, games[channel], response);
-    } else {
-      response.send('This channel does not have a game!');
-    }
-
-    // update boards[body.channel]
+    var move = parseMove(message);
+    updateGame(move, body.user_name, games[channel], channel, response);
   }
   else if (isStatus(message)) {
-    //response.send(status);
+    sendChannelResponse(response, games[channel].toString());
+  }
+  else if (isForfeit(message)) {
+    sendChannelResponse(response, games[channel].forfeit(body.user_name));     
   } else {
     response.send('Unrecognized command. Please type /ttt help for instructions.');
   }
-
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var isValid = function(body) {
+  return body.command == '/ttt' && body.token === token;
+}
+
 var isUser = function(message) {
-  return false;
+  return message[0] === '@' && message.split(' ').length === 1;
 }
 
 var getUser = function(username) {
   var payload = {
-    'token': token,
-    'user': username
+    'token': token
   };
-  r = requests.get('https://slack.com/api/users.info', params=payload)
+  r = requests.get('https://slack.com/api/users.list', params=payload);
 }
 
 var isHelp = function(message) {
   return message === 'help';
+}
+
+var validUser = function(game, user) {
+  return game.isValidUser(user);
 }
 
 var isMove = function(message) {
@@ -89,18 +103,35 @@ var isStatus = function(message) {
   return message === 'status';
 }
 
-var updateBoard = function(move, player, game, response) {
+var isForfeit = function(message) {
+  return message === 'forfeit';
+}
+
+// Update the game state and send response accordingly.
+var updateGame = function(move, player, game, channel, response) {
   if (player !== game.turn) {
     response.send('It\'s not your turn!');
     return;
   }
+  if (!game.isValidMove(move)) {
+    response.send('Invalid move!');
+    return
+  }
 
   game.move(move, player);
-  if (game.over()) {
-    // send game over
-  } else {
-    // send board and turn
+  sendChannelResponse(game.toString());
+  if (game.over) {
+    delete games[channel];
   }
+}
+
+// Send an in_channel response.
+var sendChannelResponse = function(response, string) {
+  var message = {
+    'text': string,
+    'response_type': 'in_channel'
+  }
+  response.send(message);
 }
 
 
